02fcc6c917e357f3ba0f7e7e6758b8eb3c947096
thiago.tavares <>
2019-09-03 19:45:37 -0300

    Alteração nas classes de modelo para inclusão de relacionamento

--- laravel/app/Condominio.php
+++ laravel/app/Condominio.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Condominio extends Model
 {
     protected $table = 'tb_condominio';
+    
+    public function visitante()
+    {
+        return $this->hasMany('App\Visitante');
+    }
 }
--- laravel/app/Visitante.php
+++ laravel/app/Visitante.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Visitante extends Model
 {
     protected $table = 'tb_visitante';
+    
+    public function condominio()
+    {
+        return $this->belongsTo('App\Condominio');
+    }
 }
